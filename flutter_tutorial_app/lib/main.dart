c2092f13e03c331631ca82c9fca7383c43e1e153
tehjawn <tehjawn@gmail.com>
2021-09-17 23:46:37 -0400

    Tutorial: Infinite List of Random Word Pairs

--- flutter_tutorial_app/lib/main.dart
+++ flutter_tutorial_app/lib/main.dart
@@ -9,16 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      ),
+    return const MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
@@ -31,9 +24,39 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  // Private helper function that builds our list of word pairs
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            // Adds a one-pixel high divider widget before each row
+            return const Divider();
+          }
+          final int index = i ~/ 2; // Divides i by 2 and returns integer result
+          if (index >= _suggestions.length) {
+            // Generate 10 more word pairs and add them to the suggestions list
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  // Private helper function that builds each row in the word pair list
+  Widget _buildRow(WordPair pair) {
+    return ListTile(title: Text(pair.asPascalCase, style: _biggerFont));
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
